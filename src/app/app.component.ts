a3516ce696a2d3d87b378de74ca2d161e9c8a043
ninjadev1030 <denis.dovganiuc@gmail.com>
2018-03-08 14:34:12 +0200

    update the project

---
+++ src/app/app.component.ts
@@ -0,0 +1,199 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import * as d3Scale from 'd3-scale';
+import * as d3Shape from 'd3-shape';
+import * as d3Array from 'd3-array';
+import * as d3Axis from 'd3-axis';
+
+import { Stocks } from './shared/data';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  private margin = { top: 20, right: 20, bottom: 30, left: 50 };
+  private width: number;
+  private height: number;
+  private x: any;
+  private y: any;
+  private svg: any;
+  private line: d3Shape.Line<[number, number]>;
+  private highAlert: number;
+  private lowAlert: number;
+  isDataAvailable: boolean;
+
+  constructor() {
+    this.width = 960 - this.margin.left - this.margin.right;
+    this.height = 500 - this.margin.top - this.margin.bottom;
+    this.highAlert = 5.7;
+    this.lowAlert = 5.3;
+    this.isDataAvailable = true;
+  }
+
+  ngOnInit() {
+    this.reinitSvg(this.highAlert, this.lowAlert);
+    this.initAxis();
+    this.drawAxis();
+    this.drawLine();
+  }
+
+  private initSvg() {
+    this.svg = d3.select("#visualisation")
+      .append("g")
+      .attr("width", this.width + this.margin.left + this.margin.right)
+      .attr("height", this.height + this.margin.top + this.margin.bottom)
+      .attr("id", "svg")
+      .append("g").attr("transform",
+        "translate(" + this.margin.left + "," + this.margin.top + ")");
+  }
+
+  private initAxis() {
+    this.x = d3Scale.scaleLinear().range([0, this.width]);
+    this.y = d3Scale.scaleLinear().range([this.height, 0]);
+    this.x.domain(d3Array.extent(Stocks, (d) => d.scale));
+    this.y.domain(d3Array.extent(Stocks, (d) => d.value));
+  }
+
+  private drawAxis() {
+    this.svg.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0, ' + this.height + ')')
+      .call(d3Axis.axisBottom(this.x));
+
+    this.svg.append('g')
+      .attr('class', 'axis axis--y')
+      .call(d3Axis.axisLeft(this.y))
+      .append('text')
+      .attr('class', 'axis-title')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '.71em')
+      .style('text-anchor', 'end')
+      .text('Value (data)');
+  }
+
+  private drawLine() {
+    this.line = d3Shape.line()
+      .x((d: any) => this.x(d.scale))
+      .y((d: any) => this.y(d.value));
+
+    this.svg.append('path')
+      .datum(Stocks)
+      .attr('class', 'line')
+      .attr('d', this.line)
+      .style("fill", "none")
+      .style('stroke-width', 3)
+      .style('stroke', 'rgb(0, 0, 255)');
+  }
+
+  hideChart(event: any) {
+
+    console.log('hide chart');
+
+    this.isDataAvailable = false;
+    d3.selectAll("svg > *").remove();
+  }
+
+  private redraw() {
+    d3.select("#svg").remove();
+    this.reinitSvg(this.highAlert, this.lowAlert);
+    this.initAxis();
+    this.drawAxis();
+    this.drawLine();
+  }
+
+  private reinitSvg(high, low) {
+    high = this.getY(Stocks, high);
+    low = this.getY(Stocks, low);
+    this.svg = d3.select("#visualisation")
+      .append("g")
+      .attr("width", this.width + this.margin.left + this.margin.right)
+      .attr("height", this.height + this.margin.top + this.margin.bottom)
+      .attr("id", "svg")
+      .append("g").attr("transform",
+        "translate(" + this.margin.left + "," + this.margin.top + ")");
+    this.svg.append('line')
+      .attr('x1', 0)
+      .attr('y1', high)
+      .attr('x2', this.width)
+      .attr('y2', high)
+      .style('stroke', 'rgb(255, 0, 0)')
+      .style('stroke-width', 3);
+    this.svg.append('line')
+      .attr('x1', 0)
+      .attr('y1', low)
+      .attr('x2', this.width)
+      .attr('y2', low)
+      .style('stroke', 'rgb(255, 0, 0)')
+      .style('stroke-width', 3);
+
+    this.drawCircle();
+  }
+
+  private getY(valArr, pY) {
+    var stockArr = [];
+    for (var i = 0; i < valArr.length; i++) {
+      stockArr.push(valArr[i].value);
+    }
+    var cY = this.height * (Math.max.apply(null, stockArr) - pY) / (Math.max.apply(null, stockArr) - Math.min.apply(null, stockArr));
+    return cY;
+  }
+
+  removePoint(event: any) {
+
+    var selPoint = parseFloat((<HTMLInputElement>document.getElementById("selPt")).value);
+    Stocks.splice(selPoint, 1);
+    for (var i = selPoint - 1; i < Stocks.length; i++) {
+      Stocks[i].scale = Stocks[i].scale - 1;
+    }
+
+    this.redraw();
+  }
+
+  getAlert(event: any) {
+    var highAl = parseFloat((<HTMLInputElement>document.getElementById("highAl")).value);
+
+    this.highAlert = highAl;
+
+    var lowAl = parseFloat((<HTMLInputElement>document.getElementById("lowAl")).value);
+    this.lowAlert = lowAl;
+    this.redraw();
+  }
+
+  addPoint(event: any) {
+    var addPoint = parseFloat((<HTMLInputElement>document.getElementById("addPt")).value);
+
+    var last = Stocks[Stocks.length - 1].scale;
+
+    Stocks.push({ scale: last + 1, value: addPoint });
+
+    this.redraw();
+
+    var stockArr = [];
+    for (var i = 0; i < Stocks.length; i++) {
+      stockArr.push(Stocks[i].value);
+    }
+
+    this.drawCircle();
+  }
+
+  private drawCircle() {
+    for (var j = 0; j < Stocks.length; j++) {
+      if (Stocks[j].value > this.highAlert || Stocks[j].value < this.lowAlert) {
+        var cX = this.width * j / (Stocks.length - 1);
+        var cY = this.getY(Stocks, Stocks[j].value);
+
+        this.svg.append('circle')
+          .attr('cx', cX)
+          .attr('cy', cY)
+          .attr('r', 20)
+          .style('stroke-width', 2)
+          .style("fill", "none")
+          .style('stroke', 'red');
+      }
+    }
+  }
+
+}
---
+++ src/app/shared/data.ts
@@ -0,0 +1,19 @@
+export interface Stock {
+    scale: number,
+    value: number
+}
+
+export const Stocks: Stock[] = [
+    { scale: 0, value: 5.4},
+    { scale: 1, value: 5.56 },
+    { scale: 2, value: 5.51 },
+    { scale: 3, value: 5.81 },
+    { scale: 4, value: 5.66 },
+    { scale: 5, value: 5.72 },
+    { scale: 6, value: 5.1 },
+    { scale: 7, value: 5.55 },
+    { scale: 8, value: 5.2 },
+    { scale: 9, value: 5.51 },
+    { scale: 10, value: 5.5 },
+    { scale: 11, value: 5.04}
+];
